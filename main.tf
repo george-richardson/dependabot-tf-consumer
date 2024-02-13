6390f9412ec224946b86e6c147be620238ff11af
George Richardson <george.richardson@scalefactory.com>
2024-02-13 17:16:49 +0000

    initial

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.57"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "module_a" {
+  source = "git@github.com:george-richardson/dependabot-tf-source.git//module-a?ref=module_a-v1.0.0"
+}
+
+module "module_b" {
+  source = "git@github.com:george-richardson/dependabot-tf-source.git//module-b?ref=module_b-v1.0.0"
+}
